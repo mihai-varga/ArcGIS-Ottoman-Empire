a0d107bc8b0bd1a2753c54f99fd55719560d7d2e
Mihai Varga <mihai.mv13@gmail.com>
2016-01-19 14:34:33 +0200

    history.js

---
+++ History.js
@@ -0,0 +1,92 @@
+function details(year){
+
+switch (year)	{
+
+case 1300: 
+	return "Originile imperiului Otoman isi au radacinile de pe la sfarsitul secolului 11 cand un grup de musulmani incercau sa isi apere granitele impotriva imperiului Bizantin. In anul 1299 Osman I se declara Sultan al Imperiului Otoman.";
+	break;
+case 1360: 
+	return "Orhan Ghazi a fost conducatorul Imperiului Otoman in perioada 1326-1362. El si-a concentrat obiectivele pe parte din nord-vest si a cucerit teritoriile Anatoliei.";
+	break; 
+case 1460: 
+	return "Mehmet al II-lea a fost Sultan al Imperiului Otoman pentru o scurta perioada. La numai varsta de 21 de ani a cucerit Constantinopolul sfarsind astfel Imperiul Bizantin.";
+	break;
+case 1520: 
+	return "Selim I a fost Sultan al Imperiului Otoman pana in anul 1520. In fruntea imperiului acesta a reusit sa cucereasca Orientul Mijlociu.";
+	break;
+case 1520:
+	return "Suleiman I a fost al 10-lea Sultan al Imperiului Otaman si a fost si cel care a stat la conducerea acestuia cel mai mult timp. In timpul domniei lui Imperiul Otaman a condus intr 20-30 de milioane de oameni.";
+	break;
+case 1540: 
+	return "Sub conducerea lui Suleiman I Imperiul cucereste parte din Africa de Nord pana la Algeria , tarmurile marii Mediteraneana si marii Rosii pana la golful Persic";
+	break;
+case 1560: 
+	return "Suleiman Magnificul a murit in 1566 in timpul unei expeditii catre Ungaria, moartea lui a fost tinuta secret pana cand succesorului lui Selim III a fost pus la conducere.";
+	break;
+case 1580: 
+	return "Murad III a urmat exemplul lui Selim II de a nu se duce in exepditii de cucerire, si-a pentrecut mare parte din viata in Istanbul";
+	break;
+case 1600: 
+	return "Mehmed III a fost un tip de conducator conservatist, a avut o mai mare implicare in arte";
+	break;
+case 1620:
+	return "Mustafa I a condus Imperiul Otoman in perioada 1617-1623";
+	break; 
+case 1640:
+	return "Murad IV a fost succesorul unchiului sau Mustafa I. A ajuns la conducerea imperiului la varsta de numai 11 ani. In acest timp mama lui Kosem Sultan a condus prin imtermediul acestuia.";
+	break;
+case 1660:
+	return "Mehmed IV 1648-1687, a ajuns la tron la 6 ani si a avut influenta foarte mare asupra naturii conducatorului Imperiului Otoman, renuntant la mare parte din puterea sa executiva Marelui Vizir"; 
+	break;	
+case 1700:
+	return "Mustafa II a fsot Sultanul Imperiului Otoman 1695-1703. Acesta a abdicat in favoarea fratelui sau Ahmed III";
+	break;
+case 1720:
+	return "Ahmed III a stat in fruntea Imperilui pentru aproape 27 de ani, acesta a reusit sa recastige teritoriile Azov, Morea si parte din Persia.";
+	break;
+case 1740:
+	return "Mahmud I 1730-1754, fiul lui Mustafa II. Mahmud I a incredintat guvernarea vizirilor sai si si-a pentrecut mare parte din timp scriind poezii.";
+	break;
+case 1760:
+	return "Mustafa III, fiul lui Sultan Ahmed III. El a modernizat armata si a adus-o in rand cu cele ale puterilor europene. Din punct de vedere teritorial acesta nu s-a implicat in cuceriri, tosusi spre sfarsitul domnii a declarat razboi Rusiei pe baza agrsiunilor pe care cestia le-au manifestat.";
+	break;
+case 1780:
+	return "Abdul Hamid I, a venit dupa moartea fratelui sau Mustafa III, datorita intemnitarii sale de catre verisorul si fratele lui mai mare acesta nu a aratat prea mult interes in afacerile statului.";
+	break;
+case 1800:
+ 	return "Selim III, el a introdus reforme interne pentru a intari guvernul. In principal s-a ocupat de armata,navigatioe si a deschis si o scoala in acest sens. In timpul domniei sale 2 mari conflicte au avut loc acel Austro-Turc si cel Ruso-Turc.";
+ 	break; 
+ case 1820:
+ 	return "Mahmud II, a fost al 30-lea Sultan al Imperiului Otoman. La inceputul domniei lui guvenrnatorul din Egipt Mehmet Ali Pasa a reusit sa recucereasca Medina si Mecca de la Primele State Arabe. Deasemean domnia lui a marcat prima despartire de Gracia in urma castigarii independentiei lor dupa revolta din 1821.";
+ 	break;
+ case 1840:
+ 	return "Abdulmecid I, a fost remarcat in istoria Imperiului prin ridicare miscarii nationaliste. A incercat sa intareasca relatiile cu Franta si Regatul Unit, tari cu  care au lupatat impotriva Rusiei in razboiul din Crimea. Miscarea lui a esuat din cauza faptului ca a incercat sa integreze non-musulmani si non-turci impreuna cu reformele sale."; 
+ 	break;
+ case 1860:
+ 	return "In 1862 unirea dintre Moldova si Tara Romaneasca a aavut loc sub domnia lui Alexandru Ioan Cuza ";
+ 	break;
+ case 1880:
+ 	return "In urma razboiului de independenta 1877-1878 Romania si-a dobandit independenta fata de Imperiul Otoman. Abdul Hamid II a urmat la conducerea imperiului intre ani 1876-1909. A promulgat prima constitutie otomana, dar la scurt timp din cauza presiunilor parlamentului in diferite cauze acesta a preluat puterea absoluta, dizolvan astfel constitutia anterioara.";
+ 	break;
+ case 1900:
+ 	return "In 1881 Franta ocupa Tunusia, iar Imperiul Otoman cedeaza teritorii Greciei. In 1882 guvernul din Egipt se prabuseste si acesta intra sub stapanirea Regatului Unit. Ei pierd si provinvia Bosnia in favoarea Austro-Ungariei, iar principatul Bulgariei isi declara independenta.";
+ 	break;
+ case 1920:
+ 	return "In urma razboaielor din balcani Imperiul pierde Albania,Bulgaria,Montenegro si Grecia. Totodata in aceasta perioada se termina perioada sultanilor cu Mehmed VI el fiind al 36-lea Sultan si ultimul. Primul Razboi Mondial a fost un dezastru pentru Imperiul Otoman, mare parte din imperiu a fost impartit intre marile puteri ale Europei.";
+ 	break; 
+ case 1940:
+ 	return "Nici o modificare teritoriala!";
+ 	break;
+ case 1960: 
+ 	return "Nici o modificare teritoriala!";
+ 	break;
+ case 1980:
+ 	return "Nici o modificare teritoriala!";
+ 	break;
+ case 2000:
+ 	return "Nici o modificare teritoriala!";
+ 	break; 
+default:
+	return "History";
+}
+}
